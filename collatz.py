01e946fc6f41c43d764e8c763392242cbb67d43b
Develop Intelligence <marketing@developintelligence.com>
2018-01-10 13:16:45 -0700

    Update collatz.py
    Previous version was a generator, which is too advanced for this course.  -DWS

--- collatz.py
+++ collatz.py
@@ -1,13 +1,15 @@
 def collatz(n):
-    if n < 1 or type(n) != int:
+    if type(n) != int:
+        raise TypeError
+    if n < 1:
         raise ValueError
 
     while n != 1:
+        print(n, end=' ')
         if n % 2 == 0:
            n //= 2
         else:
            n = n * 3 + 1
-        yield n
 
-    return 1
+    print(1)
     
